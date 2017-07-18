ff3cf0e820452eb6d021bbe0fec6b6607ee9f36c
alexmartinez161 <alexmartinez161@gmail.com>
2017-07-18 18:33:07 -0400

    ordering lol

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/users', users);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 //app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
